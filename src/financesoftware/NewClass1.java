6b95b464e60005315986eb2e6f6a96f2aa2f814b
niels <niels@Niels>
2013-11-02 19:52:54 +0100

    first commit

---
+++ src/financesoftware/NewClass1.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package financesoftware;
+
+/**
+ *
+ * @author melanie
+ */
+public class NewClass1 {
+    
+}
